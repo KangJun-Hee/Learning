8f39fb19c00943380a022d3d1e501018d4ab8840
강준희 <121140238+KangJun-Hee@users.noreply.github.com>
2023-05-18 17:51:28 +0900

    Add files via upload

--- Semi project/js/check.js
+++ Semi project/js/check.js
@@ -3,30 +3,39 @@ function joinUser() {
     joinBtn.addEventListener('click', function(){
 
         const username = document.getElementById('userID').value;
+        const email = document.querySelector('#email');
+        const selemail = email.options[email.selectedIndex].value;
         const password = document.getElementById('userPW').value;
         const passwordc = document.getElementById('pwc').value;
+        const name = document.getElementById('name').value;
+        const phnum = document.getElementById('phnum').value;
         const nickname = document.getElementById('nickname').value;
         const postcode = document.getElementById('rcID_adrs_w2').value;
         const adrs1 = document.getElementById('adrs1').value;
         const adrs2 = document.getElementById('adrs2').value;
+        
+        if()
     
-    
-        if (username && password && passwordc && nickname && postcode && adrs1 && adrs2) {
+        if (username && selemail && password && passwordc && name && phnum && nickname && postcode && adrs1 && adrs2) {
             if (password !== passwordc) {
                 alert('비밀번호가 일치하지 않습니다.');
                 return;
             }
-    
+
             var user = {
                 username: username,
+                selemail: selemail,
                 password: password,
+                name: name,
+                phnum: phnum,
                 nickname: nickname,
                 postcode: postcode,
                 adrs1: adrs1,
                 adrs2: adrs2
             };
-    
-            localStorage.setItem('user', JSON.stringify(user));
+            var users = JSON.parse(localStorage.getItem('users')) || [];
+            users.push(user);
+            localStorage.setItem('users', JSON.stringify(users));
     
             window.location.href = 'Login.html';
         } else {
@@ -37,40 +46,50 @@ function joinUser() {
     
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    function logcheck() {
-        alert('dd');
-    //     var idInput = document.getElementById('userID');
-    //     var pwInput = document.getElementById('userPW');
+function logcheck() {
+    var loginBtn = document.getElementById('loginbtn');
+    loginBtn.addEventListener('click', function(){
+
+        var idInput = document.getElementById('userID');
+        var pwInput = document.getElementById('userPW');
+        
+        var storedUsers = localStorage.getItem('users');
+        if(storedUsers){
+            var users = JSON.parse(storedUsers);
     
-    //    // var id = idInput.value.trim();
-    //     //var pw = pwInput.value.trim();
+            var foundUser = users.find(function(user) {
+                return user.username === idInput.value && user.password === pwInput.value;
+            });
     
-    //     var storedUser = localStorage.getItem('user');
-    //     if(storedUser){
-    //         var user = JSON.parse(storedUser);
-    
-    //         if (idInput === user.username && pwInput === user.password) {
-    //             alert('로그인 성공!');            
-    //             window.location.href = 'https://www.google.co.kr/?hl=ko';
-    //         } else {
-    //             alert('잘못된 사용자 이름 또는 비밀번호입니다.');
-    //         }
-    //     }   
-     }
-})
+            if (foundUser) {
+                alert('로그인 성공!');            
+                window.location.href = 'https://www.google.co.kr/?hl=ko';
+            } else {
+                alert('잘못된 사용자 이름 또는 비밀번호입니다.');
+            }
+        }   
+    })
+}
 
 
+function ordercheck(){
+    var oname = document.getElementById('oname');
+    var onumber = document.getElementById('onumber');
 
-
-
-
+    if(oname.value === ''){
+        alert('주문자명을 입력해주세요.');
+    }else if(onumber.value === ''){
+        alert('주문번호를 입력해주세요.');
+    }else{
+        window.location.href = 'https://www.google.co.kr/?hl=ko';
+    }
+}
 
 function emailcheck() {
-    var email = document.getElementById('id');
+    var email = document.getElementById('userID');
 
     if (email.value === '') {
-        alert('이메일을 입력해 주세요.');
+        alert('이메일을 입력해주세요.');
         email.focus();
         return false;
     } else {
@@ -93,8 +112,20 @@ function rcIDsrchf(){
 }
 
 function selectAll(selectAll){
-    const checkboxes = document.getElementsByName("agree");
+    const checkboxes = document.getElementsByName('selectAll');
+
     checkboxes.forEach((checkbox)=>{
         checkbox.checked = selectAll.checked;
     })
+}
+function checkSelectAll()  {
+    const checkboxes = document.querySelectorAll('input[name="agree"]');
+    const checked = document.querySelectorAll('input[name="agree"]:checked');
+    const selectAllCheckbox = document.querySelector('input[name="selectAll"]');
+
+    if(checkboxes.length === checked.length){
+        selectAllCheckbox.checked = true;
+    }else{
+        selectAllCheckbox.checked = false;
+    } 
 }
